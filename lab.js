4c3a4673044cbfc69c178ebafc86a2245ae00b3a
Mike Whaley <michael.d.whaley@gmail.com>
2022-11-15 21:17:47 -0500

    problem 8

--- lab.js
+++ lab.js
@@ -108,7 +108,10 @@ let passingScore = 7;
 */
 
 //CODE HERE
-
+while (score < passingScore) {
+  console.log("Your score is not high enough")
+  score++
+}
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
